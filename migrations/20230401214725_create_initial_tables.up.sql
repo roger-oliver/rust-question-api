ba22f7054d66503964484993cf2de8a1f8510392
Rogerio Oliveira <rogerio@versatility.com.br>
2023-04-07 11:17:31 +0200

    Fixed the anwser table changing the "corresponding_question" column name to "question_id";

--- migrations/20230401214725_create_initial_tables.up.sql
+++ migrations/20230401214725_create_initial_tables.up.sql
@@ -11,5 +11,5 @@ CREATE TABLE IF NOT EXISTS answers (
     id serial PRIMARY KEY,
     content TEXT NOT NULL,
     created_on TIMESTAMP NOT NULL DEFAULT NOW(),
-    corresponding_question integer REFERENCES questions
+    question_id integer REFERENCES questions
 );
